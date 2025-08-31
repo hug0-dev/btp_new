378d10d10c2d3831d97c6913d6d374d725078c33
Hugo <h.havet@icloud.com>
2025-08-31 13:08:52 +0200

    test

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
